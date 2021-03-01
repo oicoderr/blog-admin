3f583d4e5142486eaa5db85d2cbedae12f683246
dingwei <849656290@qq.com>
2021-03-01 09:54:42 +0800

    reset

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
 export const IS_DEV:boolean = process.env.NODE_ENV !== 'production'
 
-export const API_ROOT:string = IS_DEV ? 'http://127.0.0.1:8100/' : 'http://stealfood.com/'
+export const API_ROOT:string = IS_DEV ? 'http://127.0.0.1:8100/' : 'http://admin.stealfood.com/api/'
